920f4ad04e5a1ec247b5b71a36114d0e2c5379c7
PSYs0s <asmtohahere@gmail.com>
2021-04-09 14:56:10 +0600

    added

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require('express')
 
 const app = express()
-app.get('/', (req, res) => res.send('Thanks for calling'))
+app.get('/', (req, res) => {
+    const pd={
+        product:'Ginger',
+        price:200
+    }
+    res.send(pd)
+})
+app.get('/fruits/banana',(req,res)=>{
+    res.send({fruit:'banana',quantiity:1200,priice:12000})
+})
 app.listen(3000, () => console.log('Listening port 3000'))
